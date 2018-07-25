4215a709cbedfa0067bf16bd9fb97712560acc7f
Ray <ryan.natoor@gmail.com>
2018-07-25 12:03:27 +0300

    dynamic search is working #15

--- Public/index.js
+++ Public/index.js
@@ -1,13 +1,32 @@
-var input =document.getElementById("inputBox");
+var input = document.getElementById("inputBox");
 input.focus();
-input.addEventListener("keyup",function(){
+input.addEventListener("keyup", function() {
+  var jsonData;
+  var xhr = new XMLHttpRequest();
+  var url = "./src/data.json";
 
-    var xhr = new XMLHttpRequest();
-    var url = "./src/data.json";
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      jsonData = JSON.parse(xhr.responseText);
+      var keys = Object.keys(jsonData);
+      let str = input.value;
+      let sugg = document.getElementById("suggestion");
 
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState == 4 && xhr.status == 200){
-        
+      if (sugg.hasChildNodes()) {
+        while (sugg.firstChild) {
+          sugg.removeChild(sugg.firstChild);
+        }
       }
+      keys.forEach(function(k) {
+        if (str !== "" && k.indexOf(str) !== -1) {
+          let opt = document.createElement("option");
+          opt.value = k;
+          console.log(opt);
+          sugg.appendChild(opt);
+        }
+      });
     }
-})
+  };
+  xhr.open("GET", url);
+  xhr.send();
+});
